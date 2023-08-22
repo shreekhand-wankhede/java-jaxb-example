5e412e24588b75d3d1d3776f5bc04e05a5c24d05
Shreekhand Wankhede <shreekhand.wankhede@gmail.com>
2023-08-22 23:38:17 +0530

    initial commit

---
+++ src/main/java/com/example/javajaxbexample/JavaJaxbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.javajaxbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJaxbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJaxbExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javajaxbexample/JavaJaxbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javajaxbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJaxbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
